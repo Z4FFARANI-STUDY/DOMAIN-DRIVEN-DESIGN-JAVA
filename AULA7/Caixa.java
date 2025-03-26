ce2f9d0c79f18db2af3742866d4b37d2fac9b4f5
Z4ffarani <zaffarani.kaique@gmail.com>
2025-03-26 01:53:27 +0000

    AULA7

---
+++ AULA7/Caixa.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Caixa {
+    public static void main(String[] args) {
+        String nome;
+        double inicial;
+        Scanner entrada = new Scanner(System.in);
+        Random numero = new Random();
+        int conta = 1 + numero.nextInt(9999);
+
+        System.out.println("Cadastrando um novo cliente");
+        System.out.println("Digite o nome do cliente: ");
+        nome = entrada.nextLine();
+
+        System.out.println("Digite o valor inicial depositado na conta: ");
+        inicial = entrada.nextDouble();
+
+        Conta minhaConta = new Conta(nome, conta, inicial);
+        minhaConta.iniciar();
+        entrada.close();
+    }
+}
